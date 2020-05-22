30d4b84c04c66b8aee6ca05a9573aef3782acef7
bobbyleung001 <65751498+bobbyleung001@users.noreply.github.com>
2020-05-22 17:50:47 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -128,7 +128,7 @@ async function init(constraints) {
 document.querySelector('button#start').addEventListener('click', async () => {
     const constraints = {
         video: {
-            width: 1280, height: 720, frameRate: 30
+            width: 480, height: 360, frameRate: 30
         }
     };
     console.log('Using media constraints:', constraints);
