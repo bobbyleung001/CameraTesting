509ef94bf66678e534893a00f2a18e93649cec7a
bobbyleung001 <65751498+bobbyleung001@users.noreply.github.com>
2020-05-22 17:18:44 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -126,9 +126,6 @@ async function init(constraints) {
 document.querySelector('button#start').addEventListener('click', async () => {
   const hasEchoCancellation = document.querySelector('#echoCancellation').checked;
   const constraints = {
-    audio: {
-      echoCancellation: {exact: hasEchoCancellation}
-    },
     video: {
       width: 1280, height: 720
     }
